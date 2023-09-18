000f6b13e8f4ea0899d872b7e60103cfc1186a59
Matheus Gomes <matheusbgomes9@gmail.com>
2023-09-18 16:33:53 -0300

    Construção das rotas de ponto de pontos de interesse

--- descoberta_belem/settings.py
+++ descoberta_belem/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ponto_de_interesse',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- ponto_de_interesse/admin.py
+++ ponto_de_interesse/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Ponto_interesse
+
+class PontoInteresseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'descricao', 'caminho_imagem', 'localizacao')
+    list_filter = ('localizacao',)  # Optional: Add filters for specific fields
+    search_fields = ('nome', 'descricao')  # Optional: Add search fields
 
 # Register your models here.
+admin.site.register(Ponto_interesse, PontoInteresseAdmin)
