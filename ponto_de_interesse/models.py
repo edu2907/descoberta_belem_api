879974c3a9b0aa83b276637a1539e73fa62e1821
Matheus Gomes <matheusbgomes9@gmail.com>
2023-09-25 18:32:14 -0300

    Adicionar imagem para os locais
    MODIFIQUEI O ATRIBUTO CAMINHO IMAGEM PARA RECEBER IMAGEM E ADICIONEI A BIBLIOTECA DE IMAGEM + UMA PASTA ONDE APARECE AS IMAGENS ADICIONADAS (media/ponto_dei_interesse)

--- ponto_de_interesse/models.py
+++ ponto_de_interesse/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
 # Create your models here.
+#Class ponto de interesse e atributos
 class Ponto_interesse(models.Model):
     id = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=255)
     descricao = models.TextField()
-    caminho_imagem = models.CharField(max_length=255)
+    caminho_imagem = models.ImageField(upload_to='ponto_de_interesse/')
     localizacao = models.CharField(max_length=255)
+    
 
     def __str__(self):
         return self.nome
+    
