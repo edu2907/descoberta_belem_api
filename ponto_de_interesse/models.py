f49050170433734ec6e769113f2feaf31d7fc010
Matheus Gomes <matheusbgomes9@gmail.com>
2023-09-26 10:13:17 -0300

    Adicionar model do comentario
    Armazenar dados de comentarios e avaliaçoes dos usuarios.

--- ponto_de_interesse/models.py
+++ ponto_de_interesse/models.py
@@ -1,15 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
-#Class ponto de interesse e atributos
 class Ponto_interesse(models.Model):
     id = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=255)
     descricao = models.TextField()
     caminho_imagem = models.ImageField(upload_to='ponto_de_interesse/')
     localizacao = models.CharField(max_length=255)
-    
 
     def __str__(self):
         return self.nome
-    
+
+class ComentarioAvaliacao(models.Model):
+    ponto_interesse = models.ForeignKey(Ponto_interesse, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    comentario = models.TextField()
+    avaliacao = models.DecimalField(max_digits=3, decimal_places=1, default=0.0)
+
+    def __str__(self):
+        return f"Avaliação de {self.ponto_interesse.nome} por {self.usuario.username}"
